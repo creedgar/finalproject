fac547e462825b6ad79a46c88e74484258925ec6
Teni <teni.asade@yale.edu>
2022-11-29 15:35:12 -0500

    info from finance

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-# Hello
+import os
+
+from flask import Flask, flash, redirect, render_template, request, session
+
+# Configure application
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
