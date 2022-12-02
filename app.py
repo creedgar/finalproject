b9a96b849e879f96fe19a4c6afb50e3a64de69e6
Creed Gardiner <112835372+creedgar@users.noreply.github.com>
2022-12-02 12:16:32 -0500

    Deleted USD and Symbol Lookup

--- app.py
+++ app.py
@@ -11,9 +11,6 @@ from helpers import apology, login_required, lookup, usd
 # Configure application
 app = Flask(__name__)
 
-# Custom filter
-app.jinja_env.filters["usd"] = usd
-
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
