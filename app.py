4c3746e587f54939b0314440fdbed34beff9b2e3
Creed Gardiner <112835372+creedgar@users.noreply.github.com>
2022-12-08 14:14:36 -0500

    change apology

--- app.py
+++ app.py
@@ -212,18 +212,18 @@ def login():
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 403)
+            return apology()
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password", 403)
+            return apology()
 
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+            return apology()
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
@@ -258,21 +258,21 @@ def register():
 
         # Ensure password was submitted
         if not request.form.get("password"):
-            return apology("must provide password")
+            return apology()
 
         # Ensure password confirmation was submitted
         if not request.form.get("confirmation"):
-            return apology("must provide password confirmation")
+            return apology()
 
         # Query database checking for whether username is already taken
         if db.execute("SELECT username FROM users WHERE username =?", request.form.get("username")):
-            return apology("Username already taken")
+            return apology()
 
         # Check if password and confirmation match
         password = request.form.get("password")
         confirmation = request.form.get("confirmation")
         if password != confirmation:
-            return apology("Passwords do not match")
+            return apology()
 
         hash = generate_password_hash(password)
 
